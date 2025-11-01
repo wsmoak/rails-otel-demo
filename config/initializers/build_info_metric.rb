28ca858d807b9ed3a45736979be3ee3dda3e8e3f
Wendy Smoak <wsmoak@gmail.com>
2025-11-01 10:25:28 -0400

    simplify version

--- config/initializers/build_info_metric.rb
+++ config/initializers/build_info_metric.rb
@@ -1,28 +1,21 @@
-# Register a build-info observable gauge after Rails finishes initialization.
-# The gauge reports a constant quantity of 1 and includes the app version
-# as an attribute called "version".
+# Register a build_info observable gauge after Rails finishes initialization.
+# The gauge reports a constant quantity of 1 and includes the app version and process ID
 Rails.application.config.after_initialize do
-  # Determine version from the application module if available
-  version = if defined?(RailsOTelDemo) && RailsOTelDemo.const_defined?(:VERSION)
-    RailsOTelDemo::VERSION
-  else
-    # fall back to a string if we can't determine a version
-    "unknown"
-  end
-
   meter = OpenTelemetry.meter_provider.meter("rails-otel-demo-init")
+
   build_info_gauge = meter.create_observable_gauge(
     "rails_otel_demo_build_info",
-    description: "Build information for rails_otel_demo (observable gauge)",
+    description: "Build information for rails_otel_demo application",
     callback: ->(*args) {
-      Rails.logger.info "build_info callback called at #{Time.now} version=#{version} pid=#{Process.pid} tid=#{Thread.current.object_id} args_count=#{args.length}"
+      Rails.logger.debug "build_info callback called at #{Time.now} pid=#{Process.pid} tid=#{Thread.current.object_id} args_count=#{args.length}"
       1
     }
   )
 
-  Rails.logger.info "Adding attributes to build_info_gauge at #{Time.now} version=#{version} pid=#{Process.pid} tid=#{Thread.current.object_id}"
+  Rails.logger.debug "Adding attributes to build_info_gauge at #{Time.now} pid=#{Process.pid} tid=#{Thread.current.object_id}"
+
   build_info_gauge.add_attributes(
-    "version" => version,
-    "testing" => "add_attributes 903"
+    "version" => RailsOTelDemo::VERSION,
+    "process.pid" => Process.pid
   )
 end
